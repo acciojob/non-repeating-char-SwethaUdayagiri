9f6d338a6efd08721117cd6c17794b0f9ee37cf6
Swetha Udayagiri <106729744+SwethaUdayagiri@users.noreply.github.com>
2023-10-02 20:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const arr=[];
+	const a='a';
+	const output=str.charAt(0);
+	for(var i=0; i<str.length; i++){
+		if(arr[str.charCodeAt(i)-a.charCodeAt(0)]==undefined){
+			arr[str.charCodeAt(i)-a.charCodeAt(0)]=1;
+		}
+		else{
+			arr[str.charCodeAt(i)-a.charCodeAt(0)]++;
+		}
+	}
+
+	for(var i=0;i<arr.length;i++){
+		if(arr[i]==1){
+			output=String.fromCharCode(a.charCodeAt(0)+i);
+		}
+	}
+	return output;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
